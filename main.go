58022f119aca4020b43581d1fb6dc76abeddb7d3
Robert Casale <rc672@scarletmail.rutgers.edu>
2018-10-23 11:57:55 -0400

    Update main.go
    Changed the text message from "Hello world" to "Luke, I am your Father"

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World - now"}
+		res := &response{Message: "Luke, I am your FATHER - now"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
@@ -33,7 +33,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Luke, I am your FATHER - the log message")
 	})
 	http.ListenAndServe(":8080", nil)
 }
